6dfb957829c702364b29d8cc83f762e3614c636b
Azigop <aigoplay@gmail.com>
2021-10-09 14:21:25 +0300

    дз_на_генераторы

---
+++ homework/homework_04/task_generator_1.py
@@ -0,0 +1,11 @@
+def own_enumerate(arr):
+    num = 0
+    b = ()
+    for i in arr:
+        b = (num, i)
+        yield b
+        num += 1
+
+
+print(list(own_enumerate(['MAI', 'Lambda', 'Python'])))
+print(list(own_enumerate('Hello MAI!')))
